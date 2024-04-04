38c6363a2a3147e1ef6fc4e40a439d9da981bafa
Hayden Ernst <122505968+HaydenPE@users.noreply.github.com>
2024-04-04 16:10:10 -0400

    Add files via upload

--- board.cpp
+++ board.cpp
@@ -1,1 +1,63 @@
 #include "board.h"
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include "curbet.h"
+
+Board::Board(){}
+Board::~Board(){}
+void Board::ShuffleDice(){ //Creates dice for both players.
+            srand(time(NULL));
+            for(int i = 0; i < 5; i++){
+                opDice.push_back(rand() % 6 + 1);
+                totalDice.push_back(opDice.at(opDice.size()));
+                yourDice.push_back(rand() % 6 + 1);
+                totalDice.push_back(yourDice.at(yourDice.size()));
+            }
+        }
+void Board::ShuffleDiceButForTesting(){ //Same thing as ShuffleDice, but without randomizing the seed. This is to make sure we can properly use unit tests.
+            for(int i = 0; i < 5; i++){
+                opDice.push_back(rand() % 6 + 1);
+                totalDice.push_back(opDice.at(opDice.size()));
+                yourDice.push_back(rand() % 6 + 1);
+                totalDice.push_back(yourDice.at(yourDice.size()));
+            }
+
+        }
+void Board::clearDice(){ //Clears everyone's dice.
+            opDice.clear();
+            yourDice.clear();
+            totalDice.clear();
+        }
+bool Board::liar(){
+                        int amount = tablestate.getAmt();
+            int face = tablestate.getFace();
+            int compareAmt = 0;
+            for(int i = 0; i < totalDice.size(); i++){
+                if(totalDice.at(i) == face){
+                    compareAmt++;
+                }
+            }
+            if(compareAmt >= amount){
+                std::cout << "Sorry! Opponent is not a liar.\n";
+                return false;
+            }
+            else{
+            std::cout << "Opponent is a liar.\n";
+            return true;
+            }
+        }
+void Board::printDice(std::vector<int> dice){
+            for(int i = 0; i < dice.size(); i++){
+                std::cout << dice.at(i) << " \n"; 
+            }
+        }
+void Board::printDiceTest(){
+            for(int i = 0; i < opDice.size(); i++){
+                std::cout << opDice.at(i) << " \n";
+            }
+            for(int j = 0; j < yourDice.size(); j++){
+                std::cout << yourDice.at(j) << " \n";
+            }
+        }
--- curbet.h
+++ curbet.h
@@ -4,68 +4,21 @@
 
 class CurBet{
     public:
-        CurBet(){}
+        CurBet();
 
-        ~CurBet(){}
+        ~CurBet();
 
-        void changeFace(int cFace){
-            face = cFace;
-        }
+        void changeFace(int cFace);
 
-        void changeAmt(int cAmt){
-            amt = cAmt;
-        }
+        void changeAmt(int cAmt);
 
-        int getFace(){
-            return face;
-        }
+        int getFace();
 
-        int getAmt(){
-            return amt;
-        }
+        int getAmt();
 
-        bool newBet(){
-            int bFace;
-            int bAmt;
-            char l;
-            bool invalid = true;
-            bool lcall = false;
-            while(!lcall){
-                std::cout << "Would you like to call them a liar? y/n\n";
-                std::cin >> l;
-                if(l == 'y')
-                {
-                    std::cout << "LIAR!\n";
-                    return false;
-                }
-                else if(l == 'n'){
-                    lcall = true;
-                    break;
-                }
-                else{
-                    std::cout << "Oops! Invalid input.\n";
-                }
-            }
-            while(invalid){
-            std::cout << "Type in your face.\n";
-            std::cin >> bFace;
-            std::cout << "Type in the amount.\n";
-            std::cin >> bAmt;
-            if((bFace == face && bAmt > amt) || (bFace != face && bAmt > amt) || (bFace > face && bAmt == amt)){
-                changeFace(bFace);
-                changeAmt(bAmt);
-                invalid = false;
-            }
-            else{
-                std::cout << "Oops! Invalid inputs. Try again.\n";
-            }
-            }
-            std::cout << "New Current Bet (face, amount): " << bFace << " " << bAmt << "\n";
-            return true;
-        }
+        bool newBet();
 
     private:
         int face;
-        
         int amt;
 };
